c683db38f19e7069c554188462ece16e5562c09a
Dmitry Prus <jeakkeyj@gmail.com>
2022-10-10 12:17:30 +0300

    Dew 4 correct something

--- src/main/java/by/prus/gitest/Dew4Class.java
+++ src/main/java/by/prus/gitest/Dew4Class.java
@@ -3,5 +3,6 @@ package by.prus.gitest;
 public class Dew4Class {
     public static void main(String[] args) {
         System.out.println("ДЗ 4");
+        System.out.println("Сделали правки в дз 4");
     }
 }
