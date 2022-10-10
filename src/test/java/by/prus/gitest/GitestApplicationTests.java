8005ab41544783e0139bb486da33c5aa922c5ffe
Dmitry Prus <jeakkeyj@gmail.com>
2022-10-10 09:38:18 +0300

    deploy project, first commit

---
+++ src/test/java/by/prus/gitest/GitestApplicationTests.java
@@ -0,0 +1,13 @@
+package by.prus.gitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
